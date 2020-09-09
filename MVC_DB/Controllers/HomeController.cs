ba495fa7f0fcd01ed2162efee6f67b8572e2a51b
iremicyer <iremicyer@gmail.com>
2020-09-09 14:30:40 +0300

    Add project files.

---
+++ MVC_DB/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DB.Models; // model tanımlaması
+
+namespace MVC_DB.Controllers
+{
+    public class HomeController : Controller
+    {
+        //modelin içindeki veri tabanı ve tabloları kullanacağız.
+
+        urunEntities urunListe = new urunEntities();  // modelden bir nesne oluşturuldu.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Liste()
+        {
+            ViewBag.Message = "ÜRÜN LİSTEMİZ";
+
+            return View(urunListe.UrunTablo.ToList()); // yukarıda oluşturulan nesneyi burada kullanıyoruz.
+        }
+
+    }
+}
